76e0d10a9057c62b5a41bd4aec8b2f02c5e1d4c7
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-03 19:39:20 -0300

    Primeiro Comando

---
+++ Entendendo Java/PrimeirosComandos.java
@@ -0,0 +1,9 @@
+//package primeirocomandos //Comando de pacote, sendo opicional.
+
+public class PrimeirosComandos // Cabeçário da classe.
+{
+    public static void main(String[] args) // Método principal
+    {
+        System.out.println("Olá, Mundo!");
+    }
+}
