655f7e7f004574c7c2875390f6acee19df80673f
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-03 19:45:44 -0300

    Mostrar a data da máquina

---
+++ Entendendo Java/MostraDateDaMaquina.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class MostraDateDaMaquina {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A data é: " + relogio.toString());
+    }    
+}
