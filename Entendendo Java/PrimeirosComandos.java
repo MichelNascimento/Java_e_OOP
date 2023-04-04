da9d15598b8162cd8c69038eb5e2b1d5511f5bc4
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-04 12:11:29 -0300

    O que é system.out.println

--- Entendendo Java/PrimeirosComandos.java
+++ Entendendo Java/PrimeirosComandos.java
@@ -1,9 +1,19 @@
 //package primeirocomandos //Comando de pacote, sendo opicional.
 
+/* Em um programa vamos ter um método principal (MAIN);
+ * que está dentro da classe primeiro programa (CLASS);
+ * que faz parte do pacote primeiro programa (PACKAGE)
+ */
+
 public class PrimeirosComandos // Cabeçário da classe.
 {
     public static void main(String[] args) // Método principal
     {
         System.out.println("Olá, Mundo!");
+        /* "System" - Sistema
+         * "out"    - Saída
+         * "print"  - Imprimir
+         * É UM SISTEMA QUE TEM UMA SAÍDA QUE VAI IMPRIMIR
+         */ 
     }
 }
