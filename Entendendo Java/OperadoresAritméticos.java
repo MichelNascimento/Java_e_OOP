51febee6422471b11c5e5d04e379f07180c09812
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-04 13:13:21 -0300

    Atribuição

---
+++ Entendendo Java/OperadoresAritméticos.java
@@ -0,0 +1,56 @@
+public class OperadoresAritméticos {
+    public static void main(String[] args) {
+        
+        /********************/
+        /*OPERADORES UNÁRIOS*/
+        int a, b;
+
+        a++; // ou a = a + 1 (INCREMENTO)
+        a--; // ou a = a -1 (DECREMENTO)
+
+        /********************/
+        /*OPERADORES DE ATRIBUIÇÃO*/
+        a += b; // ou a = a + b (SOMAR E ATRIBUIR)
+        a -= b; // ou a = a - b (SUBTRAIR E ATRIBUIR)
+        a *= b; // ou a = a * b (MULTIPLICAR E ATRIBUIR)
+        a /= b; // ou a = a / b (DIVIDIR E ATRIBUIR)
+        a %= b; // ou a = a % b (RETOS E ATRIBUIR)
+
+        /********************/
+        /*CLASSE MATH*/
+        //Constanate
+        Math.PI; //= 3.1415...
+
+        //Exponeciação
+        Math.pow(5,2); //= 25
+
+        //Raiz quadrada
+        Math.sqrt(a); //= 5   
+
+        //Raz cúbica 
+        Math.cbrt(b); //= 3  
+
+        /********************/
+        /*ARREDONDAMENTOS*/
+        //valor absoluto
+        Math.abs(-10); // = 10
+
+        //Arredonda para baixo
+        Math.floor(3.9); // = 3
+
+        //Arredodamento para cima
+        Math.ceil(4.2); // = 5
+
+        //Arredondamento aritmeticamente
+        Math.round(5.6); // = 6
+
+        /*************************************/
+
+        //GERANDO NÚMEROS ALEATÓRIOS
+        double num = Math.random();
+        int n = (int) (1 + num * (10-1));
+
+        System.out.println(n);        
+    }
+    
+}
---
+++ Entendendo Java/OperadoresLogicosRelacionais.java
@@ -0,0 +1,63 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class OperadoresLogicosRelacionais {
+    public static void main(String[] args) {
+        
+        int n1 = 15, n2 = 9, maior;
+        if(n1 > n2){
+            maior = n1;
+        }
+        else{
+            maior = n2;
+        }
+        //EM FORMA DE OPERADOR TERNÁRIO
+        maior = n1 > n2 ? n1 : n2;
+        /*Operador Ternário possui 3 componentes:
+         *  - O uqe vem antes da interrogação (?):  n1 > n2
+         *  - O que vem depois da interrogação (?): n1 (se a condição for veraderia)
+         *  - E o que vem depois de dois pontos(:): n2 (se a condrição for falsa)
+         */
+        /****************************************************************************/
+         /*OPERADORES RELACIONAIS*/
+         (n1 > n2);  //maior que 
+         (n1 < n2);  //menor que 
+         (n1 >= n2); //maior ou igual
+         (n1 <= n2); //menor ou igual
+         (m1 == n2); //igual
+         (n1 != n2); //diferente
+
+         /**************************************************************************/
+         /*OPERADORES LÓGICOS*/
+         int x = 4, y = 7, z = 12;
+         boolean r;
+         r = (x<y && y<z) ? true : false;
+         System.out.println(r);
+
+         r = (x<y || y==z) ? true : false;
+         System.out.println(r);
+
+         r = (x<y ~ y==z) ? true : false;
+         System.out.println(r);        
+
+         r = (x<y ! y==z) ? true : false;
+         System.out.println(r);
+
+         /*************************************************************************/
+         /*COMPARANDO STRINGS */
+         String nome1 = "Fulano";
+         String nome2 = "Fulano";
+         String nome3 = new String("Fulano");
+         String resultado;
+
+         resultado = (nome1 == nome2) ? "igual" : "diferente"; //Resultado verdadeiro (igual).
+
+         resultado = (nome1 == nome 3) ? "igual" : "diferente"; //Resultado falso (diferente).
+         
+         /* O ultimo objeto "nome3" foi estanciado utilizando a clausula 'new'
+          * desta forme ela não possui a mesma estruturas dos demais, não sendo iguais"
+          * apenas o conteudo que são iguais.*/
+
+        //Utilizando o método "equals" para comparar o CONTEUDO dos objetos
+        resultado = (nome1.equals(nome3)) ? "igual" : "diferente";
+    }    
+}
