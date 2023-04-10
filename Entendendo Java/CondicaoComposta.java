6299f286d397151e2616ecdcf3c3330bd3f6d401
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-10 18:45:06 -0300

    Condição Composta

---
+++ Entendendo Java/CondicaoComposta.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CondicaoComposta {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int DataAtual = 2023;        
+        System.out.println("Digite sua data de nascimento: ");
+        int DataNasc = teclado.nextInt();
+        int calc = DataAtual - DataNasc;
+        System.out.println("Sua idade é: " + calc);
+
+        /******************************************************/
+        /*CONDIÇÃO COMPOSTA*/
+        if(calc >= 18){
+            System.out.println("Você é de MAIOR");
+        }
+        else{
+            System.out.println("Você é de MENOR");
+        }
+    }    
+}
