18d1cb49c0687541647d5429a1e40a502a131182
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-10 18:36:53 -0300

    Condição Simples

---
+++ Entendendo Java/CondicaoSimples.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CondicaoSimples {
+    public static void main(String[] args) {
+        Scanner teclado =new Scanner(System.in);
+        System.out.println("Digit s notas: ");
+
+        float n1 = teclado.nextFloat();
+        float n2 = teclado.nextFloat();
+        float media = (n1 + n2) /2;
+
+        System.out.println("Sua média foi " + media);
+
+        /*************************************************/
+        /*CONDIÇÃO SIMPLES */
+        if(media > 9){
+            System.out.println("Parabéns");
+        }  
+    }    
+}
