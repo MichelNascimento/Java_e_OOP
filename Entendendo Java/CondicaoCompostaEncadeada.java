302e854cc3b7e0449796c71302356b98091d11d8
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-10 18:55:58 -0300

    Condição Encadeada

---
+++ Entendendo Java/CondicaoCompostaEncadeada.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CondicaoCompostaEncadeada {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int DataAtual = 2023;        
+        System.out.println("Digite sua data de nascimento: ");
+        int DataNasc = teclado.nextInt();
+        int calc = DataAtual - DataNasc;
+
+        /******************************************************/
+        /*CONDIÇÃO ENCADEADA*/
+        if(calc < 16){
+            System.out.println("Sua idade é: " + calc + "\nVocê não vota");
+        }
+        else if((calc >= 16 && calc < 18) || (calc > 70)){
+        System.out.println("Sua idade é: " + calc + "\nSeu voto é OPCIONAL");
+        }
+        else{
+            System.out.println("Sua idade é: " + calc + "\nSeu voto é ORBIGATÓRIO");
+        }           
+    }    
+}
